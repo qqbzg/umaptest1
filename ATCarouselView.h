b54fe6ca5190224c68f373a21f6ae4ec3da0c8ec
liuxiyuan <liuxiyuan@abchina.com>
2018-11-10 19:10:03 +0800

    test1

---
+++ ATCarouselView.h
@@ -0,0 +1,31 @@
+//
+//  ATCarouselView.h
+//  testbed
+//
+//  Created by liuxiyuan on 2018/10/26.
+//  Copyright © 2018 Alibaba. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@class ATCarouselView;
+@protocol ATCarouselViewDelegate <NSObject>
+@optional
+/**
+ *  点击图片的回调事件
+ */
+- (void)carouselView:(ATCarouselView *)carouselView indexOfClickedImageBtn:(NSUInteger)index;
+@end
+
+@interface ATCarouselView : UIView
+//传入图片数组
+@property (nonatomic, copy) NSArray *images;
+//pageControl颜色设置
+@property (nonatomic, strong) UIColor *currentPageColor;
+@property (nonatomic, strong) UIColor *pageColor;
+//是否竖向滚动
+@property (nonatomic, assign, getter=isScrollDorectionPortrait) BOOL scrollDorectionPortrait;
+
+@property (weak, nonatomic) id<ATCarouselViewDelegate> delegate;
+@end
